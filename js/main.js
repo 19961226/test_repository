373ede7446e7b7b29783d3f87011cbca7900a7e4
19961226 <159666672+19961226@users.noreply.github.com>
2025-04-12 19:57:26 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -139,23 +139,23 @@ $(function () {
     },2500);
   });
 
-  (function(){
-    // 設定
-    var interval =3000; // 切り替わりの間隔（ミリ秒）
-    var fade_speed = 1000;// フェード処理の早さ（ミリ秒）
-    $(".fade-img-box img").hide();
-    $(".fade-img-box img:first").addClass("active").show();
+  // (function(){
+  //   // 設定
+  //   var interval =3000; // 切り替わりの間隔（ミリ秒）
+  //   var fade_speed = 1000;// フェード処理の早さ（ミリ秒）
+  //   $(".fade-img-box img").hide();
+  //   $(".fade-img-box img:first").addClass("active").show();
     
-    var changeImage = function(){
-        var $active = $(".fade-img-box img.active");
-        var $next = $active.next("img").length?$active.next("img"):$(".fade-img-box img:first");
+  //   var changeImage = function(){
+  //       var $active = $(".fade-img-box img.active");
+  //       var $next = $active.next("img").length?$active.next("img"):$(".fade-img-box img:first");
        
-        $active.fadeOut(fade_speed).removeClass("active");
-        $next.fadeIn(fade_speed).addClass("active");
-    }
+  //       $active.fadeOut(fade_speed).removeClass("active");
+  //       $next.fadeIn(fade_speed).addClass("active");
+  //   }
   
-  setInterval(changeImage,interval);
-  }());
+  // setInterval(changeImage,interval);
+  // }());
 
   /*=================================================
   top
